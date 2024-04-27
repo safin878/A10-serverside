75d0f532a3cb79f378085ad9bc7faa4d803bb893
safin878 <shafin6747@gmail.com>
2024-04-27 13:29:29 +0600

    addtoursit spot post and  get completed

--- index.js
+++ index.js
@@ -1,10 +1,73 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// ExploreAsiaParadise
+// iUBXAnk40qPeVJ5E
+
+const uri =
+  "mongodb+srv://ExploreAsiaParadise:iUBXAnk40qPeVJ5E@cluster0.c9pict7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const regCollection = client.db("UserDB").collection("Register");
+    const addCollection = client.db("UserDB").collection("AddTour");
+
+    //register api
+    app.get("/register", async (req, res) => {
+      const coursor = regCollection.find();
+      const userreg = await coursor.toArray();
+      res.send(userreg);
+    });
+
+    app.post("/register", async (req, res) => {
+      const userreg = req.body;
+      console.log(userreg);
+      const result = await regCollection.insertOne(userreg);
+      res.json(result);
+    });
+
+    //Add Tourist Spot api
+    app.get("/addtour", async (req, res) => {
+      const coursor = addCollection.find();
+      const useradd = await coursor.toArray();
+      res.send(useradd);
+    });
+    app.post("/addtour", async (req, res) => {
+      const touradd = req.body;
+      console.log(touradd);
+      const result = await addCollection.insertOne(touradd);
+      res.json(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Assingment 10 Backend");
