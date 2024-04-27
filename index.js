eedb01aeb5b3a74ca3cdc1dacec8b2d5daa58f64
safin878 <shafin6747@gmail.com>
2024-04-27 17:08:42 +0600

    deply setup completed

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const regCollection = client.db("UserDB").collection("Register");
     const addCollection = client.db("UserDB").collection("AddTour");
