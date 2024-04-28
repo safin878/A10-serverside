21928dcf1296c9749572d87f1f3e768bc343da00
safin878 <shafin6747@gmail.com>
2024-04-29 02:00:37 +0600

    single id get

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -47,6 +47,12 @@ async function run() {
       const coursor = addCollection.find();
       const useradd = await coursor.toArray();
       res.send(useradd);
+    });
+    app.get("/addtour/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addCollection.findOne(query);
+      res.send(result);
     });
     app.post("/addtour", async (req, res) => {
       const touradd = req.body;
